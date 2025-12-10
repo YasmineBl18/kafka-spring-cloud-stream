6ded2a7d68456e81b46e7b532b0d28aa0aa4fb0c
Yasmine Bourijal <yasbourijal@gmail.com>
2025-12-10 17:34:18 +0100

    Partie 2

--- src/main/java/org/example/kafkaspringcloudstream/controllers/PageEventController.java
+++ src/main/java/org/example/kafkaspringcloudstream/controllers/PageEventController.java
@@ -1,4 +1,48 @@
 package org.example.kafkaspringcloudstream.controllers;
 
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Windowed;
+import org.apache.kafka.streams.state.KeyValueIterator;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
+import org.apache.kafka.streams.state.ReadOnlyWindowStore;
+import org.example.kafkaspringcloudstream.entities.PageEvent;
+import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQueryService;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+@RestController
 public class PageEventController {
-}
+
+    private final StreamBridge streamBridge;
+    private final InteractiveQueryService interactiveQueryService;
+
+    public PageEventController(StreamBridge streamBridge, InteractiveQueryService interactiveQueryService) {
+        this.streamBridge = streamBridge;
+        this.interactiveQueryService = interactiveQueryService;
+    }
+
+    @GetMapping("publish/{topic}/{name}")
+    public PageEvent publish(@PathVariable String name, @PathVariable String topic){
+        PageEvent pageEvent = new PageEvent();
+        pageEvent.setName(name);
+        pageEvent.setDate(new Date());
+        pageEvent.setDuration(new Random().nextInt(1000));
+        pageEvent.setUser(Math.random() > 0.5 ? "U1" : "U2");
+        streamBridge.send(topic, pageEvent);
+        return pageEvent;
+    }
+
+    }
+
--- src/main/java/org/example/kafkaspringcloudstream/entities/PageEvent.java
+++ src/main/java/org/example/kafkaspringcloudstream/entities/PageEvent.java
@@ -1,4 +1,16 @@
 package org.example.kafkaspringcloudstream.entities;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class PageEvent {
+    private String name;
+    private String user;
+    private Date date;
+    private long duration;
 }
