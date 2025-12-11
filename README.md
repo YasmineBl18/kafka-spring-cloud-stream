02934eee4cbb3277f70360e4ea3972ab5806ca69
Yasmine Bourijal <yasbourijal@gmail.com>
2025-12-11 10:25:09 +0100

    Partie 4: Input Output avec Consumer Kafka

--- README.md
+++ README.md
@@ -96,5 +96,15 @@ Ensuite, l’apparition du message dans <code>kafka-console-consumer</code> conf
 ![Description de l'image](captures/5.png)
 ![Description de l'image](captures/6.png)
 
+
+
+<p>Ajoute de cette ligne dans cmd pour avoir input et output :
+docker exec -it bdcc-kafka-broker kafka-console-consumer --bootstrap-server broker:9092 --topic R4 --property print.key=true --property print.value=true --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer --property value.deserializer=org.apache.kafka.common.serialization.LongDeserializer
+
+</p>
+
+![Description de l'image](captures/7.png)
+
+
 </body>
 </html>
--- src/main/java/org/example/kafkaspringcloudstream/handlers/PageEventHandler.java
+++ src/main/java/org/example/kafkaspringcloudstream/handlers/PageEventHandler.java
@@ -1,17 +1,23 @@
 package org.example.kafkaspringcloudstream.handlers;
 
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.KStream;
 import org.example.kafkaspringcloudstream.entities.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 
 @Component
 public class PageEventHandler {
     @Bean
-    public Consumer<PageEvent> pageEventConsumer(){
-        return (input)->{
+    public Consumer<PageEvent> pageEventConsumer() {
+        return (input) -> {
             System.out.println("*****************");
             System.out.println(input.toString());
             System.out.println("*****************");
@@ -19,4 +25,26 @@ public class PageEventHandler {
         };
 
     }
+
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier() {
+        return () -> {
+            return new PageEvent(
+                    Math.random() > 0.5 ? "P1" : "P2",   // name
+                    Math.random() > 0.5 ? "U1" : "U2",   // user
+                    new Date(),                          // date
+                    10 + new Random().nextInt(10000)     // duration
+            );
+        };
+    }
+
+    @Bean
+    public Function<KStream<String, PageEvent>, KStream<String, Long>> kStreamFunction() {
+        return input ->
+                input.filter((key, value) -> value.getDuration() > 100)
+                        .map((key, value) -> new KeyValue<>(value.getName(), value.getDuration()));
+    }
 }
+
+
+
